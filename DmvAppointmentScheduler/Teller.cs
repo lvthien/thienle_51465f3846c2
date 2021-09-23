d0868aea7242ce45d21c1cfd784bc05316f5734d
lvthien <49049757+lvthien@users.noreply.github.com>
2021-09-22 19:56:34 -0500

    Add files via upload

---
+++ DmvAppointmentScheduler/Teller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DmvAppointmentScheduler
+{
+    public class Teller
+    {
+        public string id { get; set; }
+        public string specialtyType { get; set; }
+        public string multiplier { get; set; }
+
+        // My code : Adding total duration property for each teller.
+        public double totalDuration { get; set; }
+    }
+
+    public class TellerList
+    {
+        public List<Teller> Teller { get; set; }
+    }
+
+
+}
